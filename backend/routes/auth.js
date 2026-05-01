75be0c5bece862a01e2ba8e9a0d97ef36ae47025
Choi HyunJoon <hunjun71@gmail.com>
2026-05-01 22:49:28 +0900

    프로필 페이지 - 로그인한 사용자 정보 반영하도록 수정

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/me', authController.me);
+router.put('/me', authController.updateMe);
 
 module.exports = router;
